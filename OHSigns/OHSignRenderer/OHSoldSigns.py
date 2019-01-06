a12167361262c53d6bf1ca76d008e6210babd931
OrgansWithoutBodies <vsliupas@gmail.com>
2019-01-05 23:25:55 -0800

    sign templates defined

--- OHSigns/OHSignRenderer/OHSoldSigns.py
+++ OHSigns/OHSignRenderer/OHSoldSigns.py
@@ -10,20 +10,25 @@ import tempfile #makes random-named files & stores only until no longer being us
 #Text:(ratio of location on slip) - each different variable has a different font  & size
 
 #BACK
-backblurb={"Once you, the customer, have purchased an item, you are fully responsible for it. \nIf you choose to leave the item and return for it please attach this sign to it until \nyou return. Items must be picked up the same day they are purchased unless arrangements \nfor delivery through the store are made. By signing this form, you the customer \nacknowledge that Opportunity House is not responsible for any damage, re-sell, \nor loss of an item once it has been purchased, and NO REFUNDS will be given. \nDue to the minimum floor/storage space, of your item is left after close \nof the purchase date the store reserves the right to re-sell the item.":(.5,.3)}
-backmgmt={"Thank you for your cooperation \nManagement":(.23,.7)}
 fieldcorn=(.4,.8)
-backfields={"CUSTOMER NAME:":fieldcorn,"CUSTOMER SIGNATURE":tuple(map(sum,zip(fieldcorn,(0,.05)))),"PHONE #":tuple(map(sum,zip(fieldcorn,(0,.1)))), "DATE":tuple(map(sum,zip(fieldcorn,(.3,.1))))}
-            
-            
-#FRONT
-fs={"SOLD":(.5,.25)}
-fyn={"YES":(.35,.75),"NO":(.5,.75)}
-frontbtm={"*Please attach receipt if paid*":(.5,.85),"*All items must be picked up by end of day*":(.5,.95)}
-frontfields={"Date:":(.5,.05),'Cashier Initials:':(.25,.6),'Customer Initials:':(.65,.6),'Paid:':(.25,.75)}
+TEMPLATES={
+    'BACK':{
+        'backblurb':{"Once you, the customer, have purchased an item, you are fully responsible for it. \nIf you choose to leave the item and return for it please attach this sign to it until \nyou return. Items must be picked up the same day they are purchased unless arrangements \nfor delivery through the store are made. By signing this form, you the customer \nacknowledge that Opportunity House is not responsible for any damage, re-sell, \nor loss of an item once it has been purchased, and NO REFUNDS will be given. \nDue to the minimum floor/storage space, of your item is left after close \nof the purchase date the store reserves the right to re-sell the item.":(.5,.3)},
+        'backmgmt':{"Thank you for your cooperation \nManagement":(.23,.7)},
+        'backfields':{"CUSTOMER NAME:":fieldcorn,"CUSTOMER SIGNATURE":tuple(map(sum,zip(fieldcorn,(0,.05)))),"PHONE #":tuple(map(sum,zip(fieldcorn,(0,.1)))), "DATE":tuple(map(sum,zip(fieldcorn,(.3,.1))))}
+    },
+    'FRONT':{
+        'fs':{"SOLD":(.5,.25)},
+        'fyn':{"YES":(.35,.75),"NO":(.5,.75)},
+        "frontbtm":{"*Please attach receipt if paid*":(.5,.85),"*All items must be picked up by end of day*":(.5,.95)},
+        "frontfields":{"Date:":(.5,.05),'Cashier Initials:':(.25,.6),'Customer Initials:':(.65,.6),'Paid:':(.25,.75)},
+        'lblloc':{'':(.1,.1)}
+    },
+    'NEW':{
 
-lblloc={'':(.1,.1)}
-
+    }
+}
+          
 ###
 wd=os.getcwd()#folder script is stored in, used as relative point at which to save
 
@@ -34,7 +39,7 @@ def soldTag(res=100, n=(2,6),lbl=None,side='front'):
     tagsize=(round(res*8.5/n[0]),round(res*11.0/n[1]))    
     tag=Image.new('L',tagsize,color="white")
     dtag=ImageDraw.Draw(tag)
-    lbltxt={str(lbl):lblloc['']}
+    TEMPLATES['FRONT']['lbltxt']={str(lbl):TEMPLATES['FRONT']['lblloc']['']}
     def centertext(d,font,justify='center',line=0,autopar=False):        
       
         for string in d.keys():
@@ -47,17 +52,20 @@ def soldTag(res=100, n=(2,6),lbl=None,side='front'):
                 justamt=(attsize[0]/2,0)
             dtag.multiline_text([round(tagsize[i]*locrat[i]-justamt[i]) for i in range(2)],string,anchor='center',font=font)#takes the size of the tag, multiplies it by location ratio, then subtracts pixelsize of text - needed bc default action of text is to go from top-left corner, this ends up doing from the center
             #add line here
-    ubuntum=os.path.join(wd,"Fonts","ubuntu","Ubuntu-M.ttf")
+    ubuntum=os.path.join(wd,"Fonts","Ubuntu","Ubuntu-M.ttf")
+
     if side=='front':
-        centertext(fs,  font=ImageFont.truetype(ubuntum,90))
-        centertext(fyn,  font=ImageFont.truetype(ubuntum,12))
-        centertext(frontbtm,  font=ImageFont.truetype(ubuntum,12))
-        centertext(frontfields,  font=ImageFont.truetype(ubuntum,12))
-        centertext(lbltxt,  font=ImageFont.truetype(ubuntum,15))
+        frnt=TEMPLATES['FRONT']
+        centertext(frnt['fs'],  font=ImageFont.truetype(ubuntum,90)) 
+        centertext(frnt['fyn'],  font=ImageFont.truetype(ubuntum,12))
+        centertext(frnt['frontbtm'],  font=ImageFont.truetype(ubuntum,12))
+        centertext(frnt['frontfields'],  font=ImageFont.truetype(ubuntum,12))
+        centertext(frnt['lbltxt'],  font=ImageFont.truetype(ubuntum,15))
     elif side=='back':
-        centertext(backmgmt,  font=ImageFont.truetype(ubuntum,9),justify='center') 
-        centertext(backfields,  font=ImageFont.truetype(ubuntum,9),justify='center') 
-        centertext(backblurb,  font=ImageFont.truetype(ubuntum,9),justify='center') 
+        bck=TEMPLATES['BACK']
+        centertext(bck['backmgmt'],  font=ImageFont.truetype(ubuntum,9),justify='center') 
+        centertext(bck['backfields'],  font=ImageFont.truetype(ubuntum,9),justify='center') 
+        centertext(bck['backblurb'],  font=ImageFont.truetype(ubuntum,9),justify='center') 
         
         
     else:
