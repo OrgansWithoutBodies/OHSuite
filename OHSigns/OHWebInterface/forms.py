83aa9de470355f5cd89bbc8f20e5700e65afc4b3
OrgansWithoutBodies <vsliupas@gmail.com>
2019-01-08 01:23:21 -0800

    submodule'd Signs

--- OHSigns/OHWebInterface/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import formset_factory
-
-class SoldTagForm(forms.Form):#@todo gets connected to chapter/book parse function
-	nx=forms.IntegerField(min_value=1,max_value=4,label="Signs per Sheet (horizontal)",initial=2)
-	ny=forms.IntegerField(min_value=1,max_value=10,label="Signs per Sheet (vertical)",initial=6)
-	lw=forms.IntegerField(min_value=0,max_value=10,label="Width of Dividing Lines",initial=3)
-	numsheets=forms.IntegerField(min_value=1,max_value=100,label="Number of Sheets to render",initial=1)
