21077dc427434702944f528393b256c4fb72a971
OrgansWithoutBodies <vsliupas@gmail.com>
2019-01-08 21:43:32 -0800

    reorganized

---
+++ OHPickup/OHPickupAdmin/src/components/AddNewDonation.vue
@@ -0,0 +1,9 @@
+<template>
+<div>
+Item Description:
+<textarea rows="4" cols="50" class="Itemdesc"><p/>
+Donor has donated before:
+<input type='checkbox'/>
+</textarea>
+</div>
+</template>
---
+++ OHPickup/OHPickupAdmin/src/components/DonorCard.vue
@@ -0,0 +1,47 @@
+<template>
+<div class = "card">
+<b class = "name"> {{donor.lastname}}, {{donor.firstname}}</b> <p/>
+<i class = "address"> {{donor.address}}</i><p/>
+<u class = "phone"> {{donor.phone}}</u>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'DonorCard',
+  props: { donor: {
+    type: Object,
+    required: true
+  }},
+  data () {
+    return {
+      donor: donor
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card{
+background-color:#dddddd;
+border:2px solid #999999;
+position:relative;
+min-width: 100px;
+max-width: 300px;
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+transition: 0.3s;
+border-radius: 5px;
+}
+.phone {
+color: #42b983;
+}
+.phone:before{
+content:"";
+display:inline;
+width:10px;
+height:10px;
+color:#ff0000;
+}
+</style>
+<!--@todo rolodex style name tab - loc based on lastname-->
